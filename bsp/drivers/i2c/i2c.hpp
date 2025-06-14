1bec1ff057a7291f2ef10c5854898db2bef357c4
Jakub Kaszowski <harypsot@gmail.com>
2025-06-14 17:53:58 +0200

    Move I2C2 to separate dir

---
+++ bsp/drivers/i2c/i2c.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstdint>
+
+void I2C2_setup();
+uint8_t I2C2_Master_Read(uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t len);
+uint8_t I2C2_Master_Write(uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t len);
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "drivers/clock/clock.hpp"
 #include "drivers/gpio/gpio.hpp"
+#include "drivers/i2c/i2c.hpp"
 
 #include "etl/string.h"
 #include "interrupts.h"
 #include "stm32h533xx.h"
 #include "system/system.h"
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
@@ -45,92 +47,6 @@ uint8_t gotMessage = false;
 // reg_addr: Register address on the slave to read from
 // data: Pointer to data buffer
 // len: Number of bytes to read
-uint8_t I2C1_Master_Read(uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t len) {
-    uint16_t i;
-    slave_addr = slave_addr << 1;
-
-    // Check for BUSY flag
-    while ((I2C2->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY);
-
-    // Send register address (write phase)
-    I2C2->CR2 = (slave_addr << I2C_CR2_SADD_Pos) | (1 << I2C_CR2_NBYTES_Pos) | I2C_CR2_START | (0 << I2C_CR2_RD_WRN_Pos);
-
-    while ((I2C2->ISR & I2C_ISR_TXIS) == 0 && (I2C2->ISR & I2C_ISR_NACKF) == 0);
-    if (I2C2->ISR & I2C_ISR_NACKF) {
-        I2C2->ICR |= I2C_ICR_NACKCF;
-        I2C2->CR2 |= I2C_CR2_STOP;
-        return 1;
-    }
-    I2C2->TXDR = reg_addr;
-
-    // Wait for transfer complete of address
-    while ((I2C2->ISR & I2C_ISR_TC) == 0);
-
-    // Re-start condition for reading
-    // Set slave address, number of bytes (len), read direction (1)
-    I2C2->CR2 = (slave_addr << I2C_CR2_SADD_Pos) | (len << I2C_CR2_NBYTES_Pos) | I2C_CR2_START | I2C_CR2_RD_WRN;
-
-    // Read data bytes
-    for (i = 0; i < len; i++) {
-        while ((I2C2->ISR & I2C_ISR_RXNE) == 0); // Wait for RXNE
-        data[i] = I2C2->RXDR;
-    }
-
-    // Wait for transfer complete
-    while ((I2C2->ISR & I2C_ISR_TC) == 0);
-
-    // Generate STOP condition
-    I2C2->CR2 |= I2C_CR2_STOP;
-
-    while ((I2C2->ISR & I2C_ISR_STOPF) == 0);
-    I2C2->ICR |= I2C_ICR_STOPCF;
-
-    return 0; // Success
-}
-
-uint8_t I2C1_Master_Write(uint8_t slave_addr, uint8_t reg_addr, uint8_t *data, uint16_t len) {
-    uint16_t i;
-    slave_addr = slave_addr << 1;
-
-    // Check for BUSY flag (optional, can be skipped for simplicity if you manage bus contention)
-    while ((I2C2->ISR & I2C_ISR_BUSY) == I2C_ISR_BUSY);
-
-    // Set slave address (7-bit), number of bytes (1 for register address + len for data), write direction (0)
-    I2C2->CR2 &= ~I2C_CR2_RD_WRN;
-    I2C2->CR2 = (slave_addr << I2C_CR2_SADD_Pos) | ( (1 + len) << I2C_CR2_NBYTES_Pos) | I2C_CR2_START;
-
-    // Send register address
-    while ((I2C2->ISR & I2C_ISR_TXIS) == 0 && (I2C2->ISR & I2C_ISR_NACKF) == 0); // Wait for TXIS or NACKF
-    if (I2C2->ISR & I2C_ISR_NACKF) {
-        I2C2->ICR |= I2C_ICR_NACKCF; // Clear NACK flag
-        I2C2->CR2 |= I2C_CR2_STOP;   // Generate STOP condition
-        return 1; // NACK received
-    }
-    I2C2->TXDR = reg_addr;
-
-    // Send data bytes
-    for (i = 0; i < len; i++) {
-        while ((I2C2->ISR & I2C_ISR_TXIS) == 0 && (I2C2->ISR & I2C_ISR_NACKF) == 0); // Wait for TXIS or NACKF
-        if (I2C2->ISR & I2C_ISR_NACKF) {
-            I2C2->ICR |= I2C_ICR_NACKCF;
-            I2C2->CR2 |= I2C_CR2_STOP;
-            return 1; // NACK received
-        }
-        I2C2->TXDR = data[i];
-    }
-
-    // Wait for transfer complete
-    while ((I2C2->ISR & I2C_ISR_TC) == 0);
-
-    // Generate STOP condition
-    I2C2->CR2 |= I2C_CR2_STOP;
-
-    // Wait for STOPF to be set (optional, good for checking if stop happened)
-    while ((I2C2->ISR & I2C_ISR_STOPF) == 0);
-    I2C2->ICR |= I2C_ICR_STOPCF; // Clear STOPF
-
-    return 0; // Success
-}
 
 
 int
@@ -193,21 +109,9 @@ main()
   USART6->CR2 &= ~USART_CR2_STOP;
   USART6->CR2 |= USART_CR2_SWAP;
   USART6->CR1 |= USART_CR1_TE;
-
-  RCC->APB1LENR |= RCC_APB1LENR_I2C2EN;
-  I2C2->CR1 &= ~I2C_CR1_PE; // Disable I2C peripheral
-  I2C2->CR1 = 0;
-  uint16_t SCLL = 999;
-  uint16_t SCLH = 999;
-  uint16_t PRESC = 124;
-  uint16_t SDADEL = 1000;
-  uint16_t SCLDEL = 999;
-  I2C2->TIMINGR = (PRESC << I2C_TIMINGR_PRESC_Pos) |(SCLDEL << I2C_TIMINGR_SCLDEL_Pos) |(SDADEL << I2C_TIMINGR_SDADEL_Pos) | (SCLH << I2C_TIMINGR_SCLH_Pos) | SCLL;
-//  I2C2->CR1 |= I2C_CR1_DNF;
-  I2C2->CR2 = 0;
-  I2C2->CR1 |= I2C_CR1_PE;
+  I2C2_setup();
   uint8_t bq_config[1] = {1<<7 | 3 <<4 | 1 << 2};
-  I2C1_Master_Write(0x6B,0x26,bq_config, 1);
+  I2C2_Master_Write(0x6B,0x26,bq_config, 1);
   uint8_t data[16] = {};
 //  uint16_t raw_reading = 0;
   float vsys = 0;
@@ -221,7 +125,7 @@ main()
     sleep_ms(1000);
     dbgLed.toggle();
 
-    I2C1_Master_Read(0x6B, 0x30, data, 4);
+    I2C2_Master_Read(0x6B, 0x30, data, 4);
 //    raw_reading = (data[1] << 8 | data[0]) >> 1;
     vbat = ((data[1] << 8 | data[0]) >> 1) * 1.99;
     vsys = ((data[3] << 8 | data[2]) >> 1) * 1.99;
